fb570f7e71ca26b6befe09e0cb1ed10f7e775fdf
Dipu Biswas <12biswasdipu@gmail.com>
2025-09-14 20:23:03 +0530

    Enhance TopThings component styling for better layout and hover effects

--- src/components/topThings.jsx
+++ src/components/topThings.jsx
@@ -21,7 +21,7 @@ function TopThings() {
         <div className="flex justify-start md:justify-center items-center md:flex-wrap overflow-x-auto  snap-x snap-mandatory scroll-smooth  mt-6 md:mt-[40px] gap-5 ml-6 md:ml-0 px-6 md:px-0 scrollbar-hide  ">
           {items.map(({ img, names }, index) => (
             <div
-              className="snap-start shrink-0 md:bg-white rounded-2xl group"
+              className="snap-start shrink-0 md:bg-white rounded-2xl group h-[180px]  md:h-[340px]"
               key={index}
             >
               <div className="md:w-[295px] cursor-zoom-in w-[158px] h-[144px] overflow-hidden md:h-[270px] rounded-2xl  ">
@@ -31,7 +31,7 @@ function TopThings() {
                   alt={names}
                 />
               </div>
-              <h2 className="text-center group-hover:-translate-y-24 group-hover:md:text-3xl group-hover:text-white transition-all duration-500 delay-75 mt-[12px] md:mb-[30px] text-[#26395C] font-bold text-[14px] md:text-[22px] tracking-[-1%] ">
+              <h2 className="text-center cursor-pointer group-hover:md:border-8  group-hover:border-2 group-hover:mx-1 group-hover:md:mx-3 group-hover:-translate-y-24 group-hover:md:text-3xl group-hover:text-white transition-all duration-500 delay-75 mt-[12px] md:mb-[30px] text-[#26395C] font-bold text-[14px] md:text-[22px] tracking-[-1%] ">
                 {names}
               </h2>
             </div>
